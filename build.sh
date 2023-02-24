a71113c637574f3f9a1dc25f370582b9345fdaca
AutoX <32790381+AoThen@users.noreply.github.com>
2023-02-24 22:00:28 +0800

    修复

--- build.sh
+++ build.sh
@@ -132,8 +132,14 @@ target_mt7981_gl-mt2500 | \
     #自定义
     svn export https://github.com/tty228/luci-app-serverchan/trunk package/luci-app-serverchan
     svn export https://github.com/AoThen/luci-app-broadbandacc/trunk package/luci-app-broadbandacc
-    svn export https://github.com/sirpdboy/netspeedtest/trunk package/netspeedtest
+    
     # svn export https://github.com/zzsj0928/luci-app-pushbot/trunk package/luci-app-pushbot     CONFIG_PACKAGE_luci-app-pushbot=y
+
+    if [[ $istore != true ]]; 
+    then
+        svn export https://github.com/sirpdboy/netspeedtest/trunk package/netspeedtest
+    fi
+
 
     if [[ $ui == true ]]; then
         ./scripts/gen_config.py $profile glinet_depends glinet_nas custom $xadd
