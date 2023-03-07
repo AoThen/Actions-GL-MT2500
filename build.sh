a043bc762a8ca1c80fbd869bdf2a45012bdcefb8
AutoX <32790381+AoThen@users.noreply.github.com>
2023-03-07 15:46:23 +0800

    修改luci

--- build.sh
+++ build.sh
@@ -42,10 +42,7 @@ echo "Start..."
 
 #clone source tree
 git clone https://github.com/gl-inet/gl-infra-builder.git $base/gl-infra-builder
-# cd $base/gl-infra-builder
-# git reset --hard 2432dcfb4162f9cbaa816e7de3086d5a22ffea88
-# cd $base
-cp -r custom/  $base/gl-infra-builder/feeds/custom/
+# cp -r custom/  $base/gl-infra-builder/feeds/custom/
 cp -r *.yml $base/gl-infra-builder/profiles
 cd $base/gl-infra-builder
 
@@ -146,7 +143,7 @@ target_mt7981_gl-mt2500 | \
 
 
     if [[ $ui == true ]]; then
-        ./scripts/gen_config.py $profile glinet_depends glinet_nas custom $xadd
+        ./scripts/gen_config.py $profile luci glinet_depends glinet_nas custom $xadd
         git clone https://github.com/gl-inet/glinet4.x.git ~/glinet
         # cd ~/glinet
         # git reset --hard 8e665c49ca24550c53ba95bf6b705783cff90dc1
@@ -157,7 +154,7 @@ target_mt7981_gl-mt2500 | \
             cp -rf ~/glinet/pkg_config/gl_pkg_config_mt7981_mt2500.mk ~/glinet/mt7981/gl_pkg_config.mk
         fi
     else
-        ./scripts/gen_config.py $profile glinet_nas custom $xadd
+        ./scripts/gen_config.py $profile luci glinet_nas custom $xadd
     fi
     #####自定义↓↓↓
 
