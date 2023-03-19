ce8fad50f61ba57cab251c9e3e2d0a29717cb11f
AutoX <32790381+AoThen@users.noreply.github.com>
2023-03-19 09:46:42 +0800

    修改feed

--- build.sh
+++ build.sh
@@ -134,8 +134,8 @@ target_mt7981_gl-mt2500 | \
     svn export https://github.com/tty228/luci-app-serverchan/trunk package/luci-app-serverchan
     svn export https://github.com/AoThen/luci-app-broadbandacc/trunk package/luci-app-broadbandacc
 
-    svn export https://github.com/immortalwrt-collections/openwrt-cdnspeedtest/trunk/cdnspeedtest package/cdnspeedtest
-    svn export https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest/trunk/applications/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
+    # svn export https://github.com/immortalwrt-collections/openwrt-cdnspeedtest/trunk/cdnspeedtest package/cdnspeedtest
+    # svn export https://github.com/mingxiaoyu/luci-app-cloudflarespeedtest/trunk/applications/luci-app-cloudflarespeedtest package/luci-app-cloudflarespeedtest
     svn export https://github.com/AoThen/luci-app-ramfree/trunk package/luci-app-ramfree
     #git lua-maxminddb 依赖
     # git clone --depth 1 https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb
