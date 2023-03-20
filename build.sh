2d11c9a6e516680cfad972690941f50544ab3a9f
AutoX <32790381+AoThen@users.noreply.github.com>
2023-03-20 23:11:22 +0800

    glinet_nas

--- build.sh
+++ build.sh
@@ -133,7 +133,7 @@ target_mt7981_gl-mt2500 | \
             cp -rf ~/glinet/pkg_config/glinet_depends_mt2500.yml  $base/gl-infra-builder/profiles/glinet_depends.yml    #./profiles/glinet_depends.yml
         fi
 
-        ./scripts/gen_config.py glinet_depends custom $xadd
+        ./scripts/gen_config.py glinet_depends custom glinet_nas $xadd
     else
         ./scripts/gen_config.py $profile luci custom glinet_nas $xadd
     fi
