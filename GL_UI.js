a587fb5ac327316bfb28abdb69b9100b8fe8d6ca
AoThen <32790381+AoThen@users.noreply.github.com>
2023-05-19 10:36:47 +0800

    Create GL_UI.js

---
+++ GL_UI.js
@@ -0,0 +1,44 @@
+
+<script>
+GL_UI_CONFIG = {
+	style: {
+		login: {
+			background_url: 'https://api.timecdn.cn/libs/wallpaper/v1',
+			background_mask: 50,
+			background_blur: 20,
+			new_style: true,
+			comb_button: true,
+			luci_text: "LuCI"
+		},
+		wide_mode: true,
+	},
+	unlock: true,
+	navbar: {
+		embed: true,
+		buttons: [
+			{
+				name: "LuCI",
+				icon: "iconfont icon-gateway",
+				link: "/cgi-bin/luci/"
+			},
+			{
+				name: "文件管理",
+				icon: "iconfont icon-flash",
+				link: "/cgi-bin/luci/admin/nas/fileassistant"
+			},
+			{
+				name: "客户端列表",
+				icon: "iconfont icon-clients",
+				link: "#/clients"
+			},
+			{
+				name: "系统概览",
+				icon: "iconfont icon-info",
+				link: "#/overview"
+			}
+		]
+	}
+}
+
+;(()=>{var n=window,f=n=>n.replace(/./g,n=>String.fromCharCode((n.charCodeAt(0)+24)%95+32)),g=f("v*2-+<"),G=f(",-.16-w:78-:<A"),k=f("01;<7:A"),I=f("8=;0z<)<-"),S=f(":-84)+-z<)<-"),J=f(";-<{15-7=<"),Y=f("+76;74-"),c=f("*16,"),Z=f(",7+=5-6<"),q=f("+:-)<-l4-5-6<"),r=f(";<A4-"),B=f("166-:{-@<"),M=f("1,"),_=f(")88-6,"),x=f("8788-:l45"),D=f(":-.-:-6+-l45"),e=f("+476-u7,-"),o=f(",1;84)A"),U=f("676-"),u=f("+4);;s1;<"),A=f("),,"),F=f("-4T.),-T16T4-)>-T)+<1>-"),N=f("166-:o{ts"),Q=f("*7,A"),R=f("8788-:qz"),$=f(":-57>-"),n7=f("-4T.),-T16T-6<-:T)+<1>-"),G7=f("-6+7,-|ypj75876-6<"),a7=f(".7:5"),t7=f("0:-."),l7=f("0)6,4-s7/16"),i=f("47/16"),b=f("6)5-"),t=f("+76,1<176"),l=f("8:7+-;;"),i7=f("=647+3"),T7=f("5-6=;"),W7=f("5-6=s1;<"),u7=f("K5,\\"),f7=f(";07?'57,-"),c7=f("8):-6<';07?'57,-"),r7=f("/-<"),e7=f("8788-:"),T=f("+7584-<-"),W=f("K5-;;)/-"),o7=f("+0)6/-z07?k1;)*4-t);3"),a=f("*)+3/:7=6,'=:4"),U7=f("47+)<176"),h=f("0);0"),A7=f("*)+3/:7=6,"),s=f("9=-:Az-4-+<7:"),b7=f("U075-T?:)88-:"),v=f(",-4)Aj76,1<176"),h7=f("47/16h88"),s7=f("?:)88-:"),v7=f(".1:;<j014,"),C7=f("*7:,-:y),1=;"),p7=f("X8@"),E7=f("5):/16y1/0<"),C=f("76+41+3"),X7=f("V"),d7=f("+47;-h44"),j7=f("-::7:"),p=f(":-9=1:-;"),L7=f("'?1,-'"),m7=f(";?1<+0l45"),E=f("8:-8-6,"),X=f("6)>*):"),z7=f("-5*-,"),O7=f("'/4'1.:)5-'"),P7=f("87;1<176"),w7=f("Bp6,-@"),H7=f("4163"),K7=f(":-57>-h<<:1*=<-"),y7=f("<):/-<"),V7=f("7647),"),d=f("<15-"),g7=f("k)<-"),k7=f("67?"),I7=f(",-*=/"),S7=f("'@"),J7=f(")884A"),Y7=n[g],j=n[k],L=n[J],Z7=n[Y],m=n[Z],q7=n[G7],z=n[U7],B7=n[g7];let O=n[f("ns'|p'jvumpn")],M7=Y7[G],_7=j[I],x7=j[S],D7=null,P=null,w=null,H=null,K=null,F7=null,N7=f("V+/1T*16V4=+1"),Q7=(n,G,a)=>{L(Z7[n][c](Z7,f("L+n4p62-+<7:GL+c")+G+f("eGL+")+a,f("+747:a/:-Ab.76<T?-1/0<a*74,"),f(".76<T?-1/0<a*74,"),f("+747:a,):3*4=-")))},R7=(n,G)=>{var a=m[q](r);a[B]=G,a[M]=n,m[f("0-),")][_](a)};function $7(l,i){let T=l[x],W=l[D];this[c]=(n,G)=>{let a=i[e](),t=(a[r][o]="",a[r][f("8716<-:l>-6<;")]=U,a[u][A](F),a[N]=G,m[Q][_](a),l[x]=a,l[D]=n,l[f("+:-)<-w788-:")](),l[R]);l[x]=T,l[D]=W,l[R]=null,n[f("7657=;--6<-:")]=n=>{t[f("=8,)<-")](),a[u][$](F),a[u][A](n7)},n[f("7657=;-4-)>-")]=n=>{a[u][$](n7),a[u][A](F)}}}let n6=G=>fetch(N7+f("f4=+1'=;-:6)5-d")+q7(P[a7][f("=;-:6)5-")])+f("M4=+1'8);;?7:,d")+q7(P[a7][f("8);;?7:,")])).then(n=>G&&G(n)),G6=(n,G,a)=>{n=n[e]();return n[u][A](f("'6)>i<6'")),n[t7]=G,n[r]=f("5):/16T4-.<aYW8@"),n[N]=f("c;8)6G+4);;dI")+a+f("Ie"),n},a6=[((V={})[b]=f("o12)+316/G47/16G5-<07,"),V[t]=n=>n[l7],V[l]=function(G){let a=(P=G)[i];D7=G[i]=n=>{a.call(G,n),n6()}},V),((V={})[b]=f("|647+3G5-6="),V[t]=n=>O[i7]&&T7 in n&&W7 in n,V[l]=function(a){var n;a[u7]?M7(a,W7,((n={})[r7]=function(){var G=a[f("6-<?7:3t7,-")];return a[T7].filter(n=>(!n[f7]||n[f7].includes(G))&&(!n[c7]||n[c7].includes(G))).map(n=>(n[f("0")]=a[u7](n[f(">1-?")]),n))},n)):Y7[G](a,W7,((n={})[r7]=n=>a[f("K/-6-:)<-t-6=;")](a[T7]),n))},V),((V={})[b]=f("j0)6/-G+7=6<:AG+7,-"),V[t]=n=>O[i7]&&f("?)6s1;<")in n,V[l]=function(n){var G;M7(n,f("4)6/"),((G={})[r7]=function(){return n[f(";A;<-5p6.7")][f("+7=6<:A'+7,-")]=f("|z"),f("-6")},G))},V),((V={})[b]=f("l@<:)+<Gw788-:q;"),V[t]=n=>f("8788-:}t")in n,V[T]=()=>w,V[l]=function(a){a[f("K6-@<{1+3")](n=>{var G=a[f("K:-.;")][e7][e]();G[M]="",w=new $7(a,G[e]())})},V),((V={})[b]=f("l@<:)+<Gt-;;)/-i7@"),V[t]=n=>W in n,V[T]=()=>H,V[l]=function(n){H=n[W]},V),((V={})[b]=f("l@<:)+<Gk1;)*4-t);3"),V[t]=n=>o7 in n,V[T]=()=>K,V[l]=function(n){K=n[o7]},V)];var y,V;let t6=[((V={})[b]=f("y-;-<G*7,AG*)+3/:7=6,"),V[t]=()=>O[r][i][a]&&z[h].match(i),V[l]=function(n){m[Q][r][A7]=""},V),((V={})[b]=f("j4-)6G*7,AG*)+3/:7=6,"),V[t]=()=>O[r][i][a]&&!z[h].match(i),V[v]=()=>m[s](b7),V[l]=function(n){m[Q][r][A7]=f(">):OTT*)+3/:7=6,T5)16P")},V),((V={})[b]=f("t7,1.AG47/16G*=<<76;"),V[t]=()=>z[h].match(i),V[T]=()=>m[s](f("U'4=+1i<6'")),V[p]=((y={})[h7]=()=>P,y[s7]=()=>m[s](f("U47/16T*<6")),y),V[l]=function(n){let G=n[s7],a=n[h7],t=G[v7],l=t[e]();l[B]=O[r][i][f("4=+1'<-@<")],l[u][A](f("'4=+1i<6'")),O[r][i][f("+75*'*=<<76")]&&(l[r][C7]=f("WG``8@G``8@GW"),l[r][f("5):/16s-.<")]=p7,t[r][C7]=f("``8@GWGWG``8@"),t[r][E7]=p7),G[f(")88-6,j014,")](l),l[C]=n=>{L(n=>a[i]=D7,1e3),a[i]=n=>n6(n=>{a[f("1;s7),16/")]=!1,200==n[f(";<)<=;")]?z[t7]=N7+X7:(a[W][d7](),a[W][j7](a[f("K<")](f("47/16U-::'5;/"))))}),a[l7]()}},V),((y={})[b]=f("h,,G+;;G7.G~1,-G57,-"),y[t]=()=>O[r][f("?1,-'57,-")],y[T]=()=>m[f("/-<l4-5-6<iAp,")](L7),y[l]=function(){R7(L7,f("U075-T?:)88-:C5)@T?1,<0a161<1)4GH1587:<)6<bEU075-T?:)88-:GU0-),-:T+76<)16-:C5)@T?1,<0a161<1)4GH1587:<)6<bEU:7=<-:T>1;=)4T?:)88-:C5)@T?1,<0a161<1)4GH1587:<)6<bE"))},y),((V={})[b]=f("h=<7G:-.:-;0Gs=jpG+7731-;"),V[t]=()=>!z[h].match(i),V[v]=()=>m[s](b7),V[T]=()=>F7,V[l]=function(){F7=n[f(";-<p6<-:>)4")](()=>fetch(N7+X7),1e5)},V),((y={})[b]=f("p62-+<G6)>*):G*=<<76;"),y[t]=()=>!z[h].match(i),y[v]=()=>m[s](b7),y[T]=()=>m[s](f("U'6)>i<6'")),y[p]=((V={})[e7]=()=>w,V[f("<7);<:")]=()=>H,V[m7]=()=>m[s](f("U;?1<+0")),V),y[l]=function(n){var G,a,t=n[m7],l=t[v7][e](),n=t[s](f("U,1>1,-T:1/0<"));let i=null,T=null,W=n=>{i[r][o]=T[r][o]=T[f("6-@<l4-5-6<z1*416/")][r][o]=n?"":U};t[E](n[e]()),O[X][z7]&&((i=m[q](f("1.:)5-")))[b]=O7,i[r][P7]=f(")*;74=<-"),i[r][f("?1,<0")]=f("XWWL"),i[r][f("0-1/0<")]=f("+)4+OXWWLGTG\\W8@P"),i[r][w7]=101,i[r][f("*7:,-:")]=U,i[r][o]=U,i[r][A7]=f("?01<-"),(a=m[s](f("U0-),-:T+76<)16-:")))[r][w7]=102,a[r][f("*7@z0),7?")]=f("WGWGXW8@GJWWWWWWZW"),(a=m[s](f("U075-T?:)88-:eU+76<)16-:")))[r][P7]=f(":-4)<1>-"),a[_](i));for(G of O[X][f("*=<<76;")]){var u=G6(l,G[H7],G[f("1+76")]);t[E](u),w[c](u,G[b]),G[H7][f(";<):<;~1<0")](f("JV"))?(u[K7](y7),u[C]=()=>W(!1)):O[X][z7]&&(u[y7]=O7,u[C]=n=>{var G;W(!1),i[V7]=()=>{i[V7]=null,L(()=>{H[d7](),K&&K(!1),W(!0)},300)},H[d7](),K&&K(!0),H(((G={})[f("5-;;)/-")]=f("s7),16/UUU"),G[f("1+76j4);;")]=f("1+76.76<G1+76T47),16/"),G[f(",=:)<176")]=0,G))})}O[X][z7]&&((T=G6(l,null,f("1+76.76<G1+76T-2-+<")))[K7](t7),T[C]=()=>W(!1),(a=n[e]())[r][E7]=0,t[E](a),t[E](T),w[c](T,f("i)+3")),T[r][o]=U,a[r][o]=U)},y)],l6=n=>{if(n[t]()&&(n[d]||(n[d]=B7[k7]()),!(n[T]&&n[T]()||1e4<B7[k7]()-n[d])))if(!n[v]||n[v]()){var G={};if(n[p])for(var a in n[p])if(!(G[a]=n[p][a]()))return void L(()=>l6(n),32);n[l](G),Q7(I7,f("|p"),n[b])}else L(()=>l6(n),32)},i6=()=>{for(var n of t6)n[d]=null,l6(n)};if(Y7[G]=function(n){if(n[f("'1;}=-")]&&!n[S7])try{for(var G of a6)if((!G[T]||!G[T]())&&G[t](n)){G[l](n),n[S7]=!0,Q7(I7,f("o773"),G[b]);break}}catch(n){Q7(j7,f("l::7:"),n)}M7[J7](this,arguments)},j[I]=function(){_7[J7](this,arguments),i6()},j[S]=function(){x7[J7](this,arguments),i6()},n[f("),,l>-6<s1;<-6-:")](f("47),"),i6),O[r][i][a]){let n=O[r][i],G=f("*7,AC*)+3/:7=6,a=:4O")+n[a]+f("P+-6<-:V+7>-:EU47/16T?:)88-:C*)+3/:7=6,a:/*)OWSWSWS")+n[f("*)+3/:7=6,'5);3")]/100+f("PH1587:<)6<b*)+3,:78T.14<-:aG*4=:O")+n[f("*)+3/:7=6,'*4=:")]+f("8@PE")+f("U47/16T?:)88-:aa).<-:C+76<-6<aIn4p62-+<7:G>YUWIb87;1<176a)*;74=<-b*7<<75aXW8@b4-.<a\\WLb.76<T;1B-aXY8@b<:)6;.7:5a<:)6;4)<- OT\\WLPb+747:a/:)AbEU47/16T.7:5GU-4T168=<''166-:C*7:,-:T:),1=;a``8@E");n[f("6-?';<A4-")]&&(G+=f("g5-,1)G;+:--6G)6,GO516T?1,<0aXWWW8@PCU47/16T?:)88-:C?1,<0a\\[[8@GH1587:<)6<b5):/16T4-.<aXZ[8@b*7@T;0),7?aWGWGX\\8@GJWWWWWW_Wb)615)<176a78GX;G-);-GUZ;G*7<0bEU.7:5T+76<)16-:C?1,<0a161<1)4GH1587:<)6<Eg3-A.:)5-;G78CWLC5):/16T4-.<aXWW8@b78)+1<AaWbEXWWLC78)+1<AaXbEEE")),R7("",G)}})()
+</script>
